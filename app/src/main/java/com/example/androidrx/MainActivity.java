1b50776f8952666a63a99591a953caa3f47c4492
Elias Meireles <eliasmflilico@gmail.com>
2020-07-15 16:52:55 -0300

    Configuração do app center

--- app/src/main/java/com/example/androidrx/MainActivity.java
+++ app/src/main/java/com/example/androidrx/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.androidrx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "88901818-ea90-4e53-a2c2-626747008a6f",
+                Analytics.class, Crashes.class);
     }
 }
